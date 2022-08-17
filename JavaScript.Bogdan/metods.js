df714e735ae168ca9c2a7d54beb3de8705415b6e
alx <varyukhin.alx@gmail.com>
2022-08-17 16:15:27 +0400

    asa

---
+++ JavaScript.Bogdan/metods.js
@@ -0,0 +1,8 @@
+const myCity = {
+    city: 'New York',
+    cityGreeting() {
+        console.log('Greeting')
+    }
+}
+
+myCity.cityGreeting()
---
+++ JavaScript.Bogdan/object.js
@@ -0,0 +1,18 @@
+const myCity = {
+    city: 'New York',
+    info: {
+        isPopular: true,
+        country: 'USA'
+    }
+}
+
+console.log(myCity.info)
+
+const name = 'Alx'
+const postsQty = 23
+
+const userPrifile = {
+    Name,
+    postsQty,
+    hasSignedAgreement: false
+}
