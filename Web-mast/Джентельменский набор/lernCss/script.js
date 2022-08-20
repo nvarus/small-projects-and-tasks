51815c6cdd169d936878bfbf90f4d6567da265ea
alx <varyukhin.alx@gmail.com>
2022-08-20 14:44:45 +0400

    gg

--- Web-mast/Джентельменский набор/lernCss/script.js
+++ Web-mast/Джентельменский набор/lernCss/script.js
@@ -1,4 +1,19 @@
 // console.log('Hello, world')
-$(function () {
-    $("#div1").text("Hello, world");
-});
+const stopWath = () => {
+    let startTime = Date.now()
+
+    const getDelay = () => {
+        let elapsedTime = Date.now() - startTime
+        console.log(elapsedTime)
+    }
+    return getDelay
+}
+
+let timer = stopWath()
+
+// Сделать что-нибудь за некоторое время.
+for (let i = 0; i < 1000000; i++) {
+    let foo = Math.random() * 10000
+}
+// Вызвать возвращаемую функцию.
+timer()
