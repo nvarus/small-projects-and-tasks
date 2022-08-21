f7e943099a79adeb0aa75e8870de824168e23bbc
nvarus <varyukhin.alx@gmail.com>
2022-08-21 21:51:43 +0400

    asd

---
+++ JS_Задачи/Базовые/task01.js
@@ -0,0 +1,15 @@
+// Написать функцию, которая принимает два числа
+// и возращает результат их умножения
+const main = () => {
+    const FIRST_NUM = 10;
+    const SECOND_NUM = 20;
+    multiplication(FIRST_NUM, SECOND_NUM);
+}
+
+const multiplication = (FIRST_NUM, SECOND_NUM) => {
+    console.log(FIRST_NUM + SECOND_NUM);
+}
+
+main()
+
+
