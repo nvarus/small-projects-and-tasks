1d83c3425ae37929369827117e23f83672d3d5c2
alx <varyukhin.alx@gmail.com>
2022-08-24 18:19:28 +0400

    ddd

--- Codewars/JS/Convert_string_to_camel_case.js
+++ Codewars/JS/Convert_string_to_camel_case.js
@@ -7,3 +7,31 @@
 * "The_Stealth_Warrior" gets converted to "TheStealthWarrior"
 *******************************************************************************************/
 
+const ENTERING_TEXT1 = "the-stealth-warrior";
+const ENTERING_TEXT2 = "The_Stealth_Warriora";
+
+function toCamelCase(text) {
+
+    let newText = '';
+    // флаг, означающий что на следующей итерации нужно изменить регистр
+    let toUpper = false;
+
+    for (i = 0; i < text.length; i++) {
+        if (text[i] != "_" && text[i] != "-") {
+            // если найдена буква, проверяем поднят ли флаг toUpper
+            if (toUpper) {
+                newText += text[i].toUpperCase(); // если true, меняем регист
+                toUpper = false;
+            } else {
+                newText += text[i];         // иначе просто добавляем букву
+            }
+
+
+        } else { toUpper = true } // если обнаружены _ или -
+    }
+
+    console.log(newText);
+}
+
+toCamelCase(ENTERING_TEXT1);
+toCamelCase(ENTERING_TEXT2);
